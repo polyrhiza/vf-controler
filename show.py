90ee78a6deeff381b7df2549300db6cff3d12987
Matthew Fox Morgan <a1875350@adelaide.edu.au>
2025-08-29 14:58:38 +0930

    Added job select combobox by reading output from scheduler lights show

--- show.py
+++ show.py
@@ -4,7 +4,6 @@ from PySide6.QtWidgets import (QDialog, QHBoxLayout, QVBoxLayout, QLabel, QAppli
 from PySide6.QtGui import QFont
 import time
 import re
-import string
 
 
 class ShowMainWindow(QDialog):
@@ -48,6 +47,21 @@ class ShowMainWindow(QDialog):
 
         # SECOND ROW IS IN MAIN LAYOUT AND IS THE TEXT OUTPUT
 
+        third_row_widget = QWidget()
+        third_row_layout = QHBoxLayout()
+
+        self.remove_schedule_text = QLabel('To Remove a Light Schedule Select a Job:')
+        self.remove_schedule_text.setVisible(False)
+
+        self.job_select_combo_box = QComboBox()
+        self.job_select_combo_box.setVisible(False)
+
+        third_row_layout.addWidget(self.remove_schedule_text)
+        third_row_layout.addWidget(self.job_select_combo_box)
+        third_row_widget.setLayout(third_row_layout)
+
+
+
         #######################################
         #            MAIN LAYOUT              #
         #######################################
@@ -61,6 +75,7 @@ class ShowMainWindow(QDialog):
         main_layout = QVBoxLayout()
         main_layout.addWidget(first_row_widget)
         main_layout.addWidget(self.show_output)
+        main_layout.addWidget(third_row_widget)
         main_layout.addStretch(1)
 
         self.setLayout(main_layout)
@@ -75,12 +90,11 @@ class ShowMainWindow(QDialog):
         if show_item == "command history":
             show_item = "history"
 
-        print(show_item)
-        print(f'{show_item} show' + '\n')
-
+        # DUMP PREVIOUS RETRIEVALS STILL SITTING IN LIMBO
         while self.shell.recv_ready():
             self.shell.recv(4096)
 
+        # SEND COMMAND
         self.shell.send(f'{show_item} show' + '\n')
 
         output = ''
@@ -91,15 +105,19 @@ class ShowMainWindow(QDialog):
         last_recv_time = time.time()
         full_command = f'{show_item} show'
 
+        # LOOP FOR RETRIEVING OUTPUT
         while True:
+            # CONTINUE TO LOOP WHILE THE SHELL IS READY
             while self.shell.recv_ready():
                 output += self.shell.recv(4096).decode(errors='ignore')
                 last_recv_time = time.time()
 
+            # IF RECV_READY IS SHOWING FALSE FOR A SHORT PERIOD THE LOOP IS BROKEN I.E. NO MORE DATA TO RETRIEVE
             if time.time() - last_recv_time > quiet_period:
                 print('No more data to recieve.')
                 break
 
+            # IF AN ERROR OCCURS AND RECV_READY IS STUCK IN TRUE TIMES OUT AFTER 15 SECONDS
             if time.time() - start_time > timeout:
                 print('Timeout waiting for shell')
                 print('Timeout output:\n', output)
@@ -107,51 +125,37 @@ class ShowMainWindow(QDialog):
 
             time.sleep(0.1)
 
-        print("Raw Output:\n", output)
-        print('End Raw Output')
-
-        # CLEAN THE OUTPUT
 
         # STRIP ANSI CODES
         ansi_codes = re.compile(r'(?:\x1B[@-_][0-?]*[ -/]*[@-~])')
         stripped_output = ansi_codes.sub('', output)
         print(f'Stripped output:\n {stripped_output}')
 
+        # STRIPPING UNECESSARY SPACES AT END OF LINES
         stripped_output_lines = []
         for line in stripped_output.splitlines():
             stripped_line = line.rstrip()
             stripped_output_lines.append(stripped_line)
 
-
-        # for line in stripped_output.splitlines():
-        #     cleaned_line = ''
-        #     for c in line:
-        #         if c in string.printable:
-        #             cleaned_line += c
-        #         else:
-        #             pass
-        #     cleaned_line = cleaned_line.rstrip()
-        #     stripped_output_lines.append(cleaned_line)
-
+        # THE SENT COMMAND SEES THE INITIAL SHELL PROMPT AND SENT COMMAND. THIS FINDS ITS INDEX
         last_seen_command_idx = 0
         for i, line in enumerate(stripped_output_lines):
             if full_command in line:
                 last_seen_command_idx = i + 1
 
+        # THIS REMOVES THE INITIAL SHELL PROMPT FORM THE OUTPUT
         stripped_output = stripped_output_lines[last_seen_command_idx:]
 
+        # THE OUTPUT ALSO CONTAINS THE SHELL PROMPT THAT APPEARS AFTER THE OUTPUT. THIS FINDS ITS INDEX
         last_seen_marker = 0
         for i, line in enumerate(stripped_output):
             if marker in line:
                 last_seen_marker = i
 
+        # THIS REMOVES ITS INDEX
         stripped_output = stripped_output[:last_seen_marker]
 
-
-        # if stripped_output and marker in stripped_output_lines:
-        #     stripped_output_lines.pop()
-
-        # stripped_output = stripped_output_lines[last_seen_command_idx:]
+        # JOINS BACK TOGETHER LINES
         final_output = "\n".join(stripped_output)
 
         print(f'Final output:\n {final_output}')
@@ -159,8 +163,33 @@ class ShowMainWindow(QDialog):
         # SET SHOW_OUTPUT AS THE OUTPUT OF THE SHELL
         self.show_output.setText(final_output)
 
+        # ANOTHING DUMPING OF WAITING DATA. JUST INCASE.
         while self.shell.recv_ready():
             self.shell.recv(4096)
+
+        # SEND TO THE FUNCTION THAT REMOVES A JOB.
+        if show_item == 'scheduler lights':
+            self.RemoveLightSchedule(output=final_output)
+
+    def RemoveLightSchedule(self, output=None):
+
+        # SETTING OPTIONS AS VISIBLE
+        self.remove_schedule_text.setVisible(True)
+        self.job_select_combo_box.setVisible(True)
+
+        lines = output.splitlines()
+
+        jobs = []
+        for line in lines:
+            match = re.match(r'^Job \d+', line)
+            if match:
+                jobs.append(match.group(1))
+
+        for i in jobs:
+            self.job_select_combo_box.addItem(i)
+
+
+
 
 # app = QApplication(sys.argv)
 # window = ShowMainWindow()
